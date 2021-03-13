f7065182478931f6630c840a23e501d95baec324
con0fav <connor.favero@gmail.com>
2021-03-13 11:48:27 -0800

    building tables

--- db/officeDB.sql
+++ db/officeDB.sql
@@ -2,3 +2,23 @@ DROP DATABASE IF EXISTS office_DB;
 CREATE DATABASE office_DB;
 
 USE office_DB;
+
+CREATE TABLE department (
+    id INT PRIMARY KEY,
+    name VARCHAR(30)
+);
+
+CREATE TABLE role (
+    id INT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INTEGER
+);
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT
+);
