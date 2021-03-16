c52eb9f19c77bfeb5ab4acefca26dae750b2ef63
con0fav <connor.favero@gmail.com>
2021-03-15 21:36:42 -0700

    buidling database

--- db/seed.sql
+++ db/seed.sql
@@ -18,17 +18,17 @@ VALUES ("Lawyer", 120000, 2);
 INSERT INTO role (title, salary, department_id)
 VALUES ("Paralegal", 125000, 2);
 INSERT INTO role (title, salary, department_id)
-VALUES ("Team Lead", 250000, 4);
+VALUES ("Team Lead", 250000, 3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Ann", "Nonymous", 1, 3);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Bill", "Guy", 2, 1);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Reel", "Persohn", 3, 4);
+VALUES ("Reel", "Persohn", 3, 2);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Earl", "Gray", 4, 3);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Charls", "Charge", 5, 4);
+VALUES ("Charls", "Charge", 5, 2);
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Greg", "Goodbody", 2, null);
