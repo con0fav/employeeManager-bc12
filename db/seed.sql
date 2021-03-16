bcd797e4d279e3fbec7282794c87bcf3d40d1145
con0fav <connor.favero@gmail.com>
2021-03-15 17:20:43 -0700

    building seeds

---
+++ db/seed.sql
@@ -0,0 +1,34 @@
+USE office_DB;
+
+INSERT INTO department (name)
+VALUES ("Accounting");
+
+INSERT INTO department (name)
+VALUES ("Legal");
+
+INSERT INTO department (name)
+VALUES ("Management");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant", 100000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Bean Counter", 150000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lawyer", 120000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Paralegal", 125000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Team Lead", 250000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Ann", "Nonymous", 1, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Bill", "Guy", 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Reel", "Persohn", 3, 4);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Earl", "Gray", 4, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Charls", "Charge", 5, 4);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Greg", "Goodbody", 2, null);
